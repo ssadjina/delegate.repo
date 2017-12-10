9e6ab1723828ff40f4f010488e05f8b8474dc8d3
ssadjina <30830561+ssadjina@users.noreply.github.com>
2017-12-10 22:24:41 +0100

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -2,22 +2,32 @@ var app = angular.module('delegateApp', []);
 
 app.controller('indexCtrl', function($scope, $http) {
     $scope.accounts = [];
+    $scope.standby = [];
     $scope.lastpayout = 0;
     $scope.nextpayout = 0;
 
     $http.get ('poollogs.json').then (function (res) {
         $scope.lastpayout = res.data.lastpayout * 1000;
-        $scope.nextpayout = moment ($scope.lastpayout).add (1, 'week').valueOf();
+        $scope.nextpayout = moment ($scope.lastpayout).add (1, 'day').valueOf();
         $scope.accounts = [];
+        $scope.standby = [];
+        $scope.weight = res.data.weight;
+        $scope.riseperday = res.data.riseperday;
 
         for (addr in res.data.accounts) {
             var it = res.data.accounts[addr];
             it['address'] = addr;
             $scope.accounts.push (it);
         }
+
+        for (addr in res.data.standby) {
+            var it = res.data.standby[addr];
+            it['address'] = addr;
+            $scope.standby.push (it);
+        }
     });
 
-    $http.get ('https://wallet.shiftnrg.org/api/delegates/get?username=seatrips').then (function (res) {
+    $http.get ('https://wallet.oxycoin.io/api/delegates/get?username=XXX').then (function (res) {
         $scope.delegate = res.data.delegate;
     });
 });
